ee2f63d6bc5fc2506fb3fd05ceca23ac52aca615
Filipe Chagas <filipe@ochagas.com>
2012-11-19 20:20:10 -0200

    created gem structure

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Starenergy::Application.config.secret_token = '837e7ea1aca3e737d29768640bf8e991a9e16477077c2faa10eced0e4c373d9d2ae9f0d79efc6b530180fc982c44a54cb8b6dbc52df1b636d00d35eb7bd143d1'
